307aa86a986095455247a67f431ae6c80103ddc8
microcai <microcai@fedoraproject.org>
2010-10-23 11:33:33 +0800

    make index page static

--- src/http_server/http_server.c
+++ src/http_server/http_server.c
@@ -26,9 +26,6 @@
 #include "smsapi.h"
 #include "html_paths.h"
 
-static void SoupServer_path_index(SoupServer *server, SoupMessage *msg,
-		const char *path, GHashTable *query, SoupClientContext *client,
-		gpointer user_data);
 static void SoupServer_path_root(SoupServer *server, SoupMessage *msg,
 		const char *path, GHashTable *query, SoupClientContext *client,
 		gpointer user_data);
@@ -61,8 +58,8 @@ int start_server()
 
 	soup_server_add_handler(server,"/login",SoupServer_path_login,NULL,NULL);
 
-	soup_server_add_handler(server,"/index.html",SoupServer_path_index,NULL,NULL);
-	soup_server_add_handler(server,"/index.htm",SoupServer_path_index,NULL,NULL);
+//	soup_server_add_handler(server,"/index.html",SoupServer_path_index,NULL,NULL);
+//	soup_server_add_handler(server,"/index.htm",SoupServer_path_index,NULL,NULL);
 	soup_server_add_handler(server,"/info",SoupServer_path_info,NULL,NULL);
 
 	soup_server_add_handler(server,"/getsmscode.asp",SoupServer_path_getsmscode,NULL,NULL);
@@ -80,61 +77,13 @@ int start_server()
 	return soup_server_get_port(server);
 }
 
-
-static void SoupServer_path_index(SoupServer *server, SoupMessage *msg,
-		const char *path, GHashTable *query, SoupClientContext *client,
-		gpointer user_data)
-{
-	soup_message_set_status(msg, SOUP_STATUS_OK);
-
-	soup_message_headers_set_content_type(msg->response_headers, "text/html; charset=UTF-8",
-			NULL);
-	soup_message_headers_set_encoding(msg->response_headers,
-			SOUP_ENCODING_CONTENT_LENGTH);
-
-	HtmlNode * html = htmlnode_new(NULL,"html",NULL);
-
-	HtmlNode * head = htmlnode_new_head(html, NULL);
-
-	htmlnode_new(head,"link","rel=\"shortcut icon\"","href=\"/favicon.ico\"","type=\"image/x-icon\"",NULL);
-
-	htmlnode_new_text(htmlnode_new(head,"title",NULL),"登录以使用网络");
-
-	htmlnode_new_js(head,"function btClick(){"
-			"window.open('/login.monitor','loginwin','toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=500,height=300');"
-			"loginform.action=\"/login.monitor\";loginform.submit();}");
-
-	HtmlNode * body = htmlnode_new_body(html,NULL);
-
-	HtmlNode * tr = htmlnode_new(htmlnode_new_table(htmlnode_new_form(body,"POST","/login.monitor","name=\"loginform\"","target=\"loginwin\"",NULL),"border=\"0\"","align=\"center\"",NULL),"tr",NULL);
-
-	htmlnode_new_text(htmlnode_new(htmlnode_new(tr,"td",NULL),"p",NULL),"输入验证码:");
-
-	htmlnode_new(htmlnode_new(tr,"td",NULL),"input","type=\"text\"","name=\"id\"",NULL);
-
-
-	htmlnode_new(htmlnode_new(tr,"td",NULL),"input","type=\"submit\"","value=\"验证\"","name=\"Submit\"","onclick=\"btClick()\"",NULL);
-
-
-	gchar * smsurl = g_key_file_get_string(gkeyfile, "http", "sms_url", NULL);
-
-	if (smsurl)
-	{
-		htmlnode_new_iframe(body,smsurl,"height=\"80%\"","width=\"100%\"","name=\"smsframe\"",NULL);
-		g_free(smsurl);
-	}
-	htmlnode_to_plane_text_and_free(html,(htmlnode_appender)soup_message_body_appender,msg->response_body);
-
-	soup_message_body_complete(msg->response_body);
-}
-
 static void SoupServer_path_root(SoupServer *server, SoupMessage *msg,
 		const char *path, GHashTable *query, SoupClientContext *client,
 		gpointer user_data)
 {
 	if(g_strcmp0(path,"/")==0)
 	{
-		return SoupServer_path_index(server,msg,path,query,client,user_data);
+		return SoupServer_path_static_file(server,msg,"/index.html",query,client,user_data);
 	}
 
 	return SoupServer_path_static_file(server,msg,path,query,client,user_data);
